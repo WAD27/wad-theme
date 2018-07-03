9a685392d9342b6cb186879c7c4cca9b73667d22
josecaos <jazzvoon@gmail.com>
2018-07-03 01:49:29 -0500

    Nuevos archivos + primera estructura php header + body + footer

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+include "header.php";
+
+
+
+include "inicio.php";
+
+
+
+include "footer.php";
---
+++ inicio.php
@@ -0,0 +1,13 @@
+
+
+    <div class="grid-container">
+
+      <div class="grid-x text-center align-middle">
+
+        <h1 class="cell">WAD</h1>
+
+      </div>
+
+
+
+    </div>
