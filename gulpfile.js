6aab95b7a50296510e39d859aae70877e756d643
josecaos <jazzvoon@gmail.com>
2018-07-06 17:27:42 -0500

    Cambio de librerias - foundation por bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,40 @@
-var gulp = require('gulp');
-var $    = require('gulp-load-plugins')();
-
-var sassPaths = [
-  'node_modules/foundation-sites/scss',
-  'node_modules/motion-ui/src'
-];
+// continuar con la configuracion de js.min y uglify
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+var browserSync = require('browser-sync').create()
+var useref = require('gulp-useref')
 
 gulp.task('sass', function() {
   return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('css'));
-});
+  // return gulp.src('scss/**/*.scss')//accept any file in scss folder
+  .pipe(sass())
+  .pipe(gulp.dest('css/'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
+})
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
-});
+gulp.task('browserSync', function() {
+  browserSync.init({
+    // server: { //for html as index
+    //   basedir: 'app/'
+    // },
+    proxy: 'http://localhost/ethbinary',// for php as index
+    options: {
+      reloadDelay: 150
+    },
+  })
+})
+
+// gulp.task('useref', function() {
+//   return gulp.src('*.php')
+//   .pipe(useref())
+//   .pipe(gulp.dest('./js/dist/'))
+// })
+
+gulp.task('watch', ['browserSync','sass'/*,'useref'*/]/*order*/, function() {
+  gulp.watch('scss/**/*.scss',['sass'])
+  gulp.watch('js/**/*.js', browserSync.reload)
+  gulp.watch('sections/**/*.php', browserSync.reload)
+  gulp.watch('*.php', browserSync.reload)//for php as index
+})
